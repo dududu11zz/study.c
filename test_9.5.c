27bd545fa4a3e3153d7fe7782b43334480b62019
郑云海 <112851862+dududu11zz@users.noreply.github.com>
2022-09-06 21:50:04 +0800

    Add files via upload

---
+++ test_9.5.c
@@ -0,0 +1,66 @@
+//分支语句（选择结构） if ; switch
+//循环语句 while ; for ; do while
+#include<stdio.h>
+
+
+
+
+
+//int main()
+//{
+//	int age = 150;
+//
+//	if (age < 18)
+//	{	printf("未成年\n");
+//      printf("不能谈恋爱\n");
+//  }//if语句不能执行多条语句
+//   //若要执行，应该使用代码块（{}）  
+//	else
+//	{
+//		 if (age >= 18 && age < 28)
+//		printf("青年\n");
+//		else if (age >= 28 && age < 50)
+//		printf("壮年\n");
+//		else if (age >= 50 && age < 90)
+//		printf("老年\n");
+//		else
+//		printf("寿比南山\n");
+//	}
+//
+//
+//
+//	/*int age = 150;
+//
+//	if (age < 18)
+//		printf("未成年\n");*/
+//	/*else if (age >= 18 && age < 28)
+//		printf("青年\n");
+//	else if (age >= 28 && age < 50)
+//		printf("壮年\n");
+//	else if (age >= 50 && age < 90)
+//		printf("老年\n");
+//	else
+//		printf("寿比南山\n");*/
+//
+//
+//
+//
+//	/*if (age < 18)
+//		printf("未成年\n");
+//	else
+//		printf("成年\n");*/
+//
+//
+//	/*if (age < 18)
+//		printf("未成年\n");*/
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;//;隔开每条语句
+//	;//此时同样是语句--空语句
+//
+//	return 0;
+//}
