888169e6c7fc58f9c199838554bc6108d6b2fb0d
郑云海 <112851862+dududu11zz@users.noreply.github.com>
2022-09-11 23:59:52 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,52 +1,138 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	int i, k;
-	for (i = 0, k = 0; k = 0; i++, k++)//中间判断 k为假，所以循环结束，一共进行0次循环
-		k++;
+	int 
+
 	return 0;
 }
 
-//int main()
+
+
+//int main()//利用二分查找法查找(有序数组)
 //{
-//	int x, y;
-//	for (x = 0, y = 0; x < 2 && y < 5; ++x, y++)//&&表示并且，++x与x++效果相同
+//	
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(1);
+//	int k = 9;
+//	int left = 0;
+//	int right = sz - 1;
+//	
+//	while (left <= right)
 //	{
-//		printf("hehe\n");
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标为：%d\n", mid);
+//			break;
+//		}
 //	}
+//	if (left > right)
+//		printf("找不到\n");
 //	return 0;
 //}
 
-//int main()
+
+//int main()//查找有序数组中值的下标。
 //{
-//	int i = 0, j = 0;
-//	for (; i < 10; i++)
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 7;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(1);
+//	for (i = 0; i < sz; i++)
 //	{
-//		for (; j < 10; j++)
+//		if (k == arr[i])
 //		{
-//			printf("hehe\n");//此时只会打印10个呵呵，因为此时的j并未初始化，所以当i=0时，程序往下走
-//			                 //此时打印10个hehe，并且此时j=10；当i=1在往下时，因为j没有初始化，所以
-//			                 //j还是等于10，不在打印hehe，所以一共就10给hehe
+//			printf("找到了，下标为：%d", i);
+//			break;
 //		}
 //	}
+//	if (i == sz)
+//		printf("找不到");
+//	return 0;
+//}
+
+//int main()//计算n的阶乘的相加
+//{
+//	int i = 1;
+//		int n = 0;
+//		int c = 1;//定义c为n的阶乘
+//		int sum = 0;
+//		scanf_s("%d", &n);
+//		for (i = 1; i <= n; i++)
+//		{
+//			c = i *c;
+//			sum = sum + c;
+//		}
+//		printf("%d ", sum);
+//	
+//	return 0;
+//}
+
+//int main()//计算n的阶乘
+//{
+//	int i = 1;
+//	int n = 0;
+//	int c = 1;//定义c为n的阶乘
+//	int sum = 0;//定义sum为1！+2!+ .. +10!
+//	
+//	for (n = 1; n <= 3; n++)
+//	{
+//		for (i = 1,c = 1; i <= n; i++)//要定义c的初始化
+//		{
+//			
+//			c = i * c;
 //
+//		}
+//		sum = sum + c;
+//	}
+//	
+//	printf("%d ", sum);
 //
 //	return 0;
 //}
 //
-//
-//int main()
+
+//int main()//计算n的阶乘
 //{
-//	int i = 0, j = 0;
-//	for (i = 0; i < 10; i++)
+//	int i = 1;
+//	int n = 0;
+//	int c = 1;//定义c为n的阶乘
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
 //	{
-//		for (j = 0; j < 10; j++)
-//		{
-//			printf("hehe\n");//此时会打印100个呵呵
-//		}
+//		c = i *c;
+//		
 //	}
+//	printf("%d ", c);
 //
+//	return 0;
+//}
+
+ //do
+ //  循环语句；
+	// while（表达式）；
+
+//int main()
 //
+//{
+//	int i=0;
+//	do
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	while (i <= 10);
 //	return 0;
 //}
