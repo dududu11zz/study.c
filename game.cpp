817b8b9df18571a2556a6dc0c31039bd731e506e
郑云海 <112851862+dududu11zz@users.noreply.github.com>
2022-10-06 23:03:14 +0800

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,39 +1,119 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
+#include"game.h"
 
-void menu()
+void InitBoard(char board[ROW][COL], int row, int col)
 {
-	printf("***********************************\n");
-	printf("********1. play    0. exit*********\n");
-	printf("***********************************\n");
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for(j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
 }
 
-void test()
+void displayboard(char board[ROW][COL], int row, int col)
 {
-	int input;
-	do
+	int i = 0;
+	//1.打印一行的数据
+	for (i = 0; i < row; i++)
 	{
-		menu();
-		printf("请选择:>");
-		scanf("%d", &input);
-		switch (input)
+		
+		int j = 0;
+		for (j = 0; j < col; j++)
 		{
-		case 1:
-			printf("三子棋\n");
-			break;
-		case 0:
-			printf("退出游戏");
-			break;
-		default:
-			printf("选择错误，请重新选择");
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+				printf("|");
+			
+		}
+		printf("\n");
+		//2.打印分割行
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if(j<col-1)
+				printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+//void displayboard(char board[ROW][COL], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		//1.打印一行的数据
+//		printf(" %c | %c | %c \n",board[i][0],board[i][1],board[i][2]);
+//		
+//		//2.打印分割行
+//		if(i<row-1)
+//		printf("---|---|---\n");
+//	}
+//
+//}
+
+
+void playermove(char board[ROW][COL], int row, int col)
+{
+	int x;
+	int y;
+	printf("玩家落子:>\n");
+	
+	while (1)
+	{
+		printf("请输入要下的坐标:>");
+		scanf("%d%d", &x, &y);
+		//判断x,y坐标的合理性
+		if (x >= 1 && x <=col && y >= 1 && y <= row)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("该坐标被占用\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入!\n");
+		}
+	}
+}
+
+void computermove(char board[ROW][COL], int row, int col)
+{
+	printf("电脑走:>\n");
+	int x; int y;
+	x = rand() % row;
+	y = rand() % col;
+	while (1)
+	{
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
 			break;
 		}
-	} while (input);
-
+	}
 }
 
-int main()
+char iswin(char board[ROW][COL], int row, int col)
 {
-	test();
-	return 0;
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0])
+		{
+
+		}
+	}
 }
--- test_10_5.cpp
+++ test_10_5.cpp
@@ -10,10 +10,42 @@
 
 void game()
 {
+	char ret;
 	//创建一个数组，存放走出的棋盘信息
 	char board[ROW][COL] = { 0 };//全部空格
 	//初始化棋盘
 	InitBoard(board, ROW, COL);
+	//打印棋盘
+	displayboard(board,ROW,COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		playermove(board,ROW,COL);
+		displayboard(board, ROW, COL);
+		//判断玩家是否赢
+		ret=iswin();
+		if (ret != 'c');
+		{
+			break;
+		}
+		//电脑下棋
+		computermove(board, ROW, COL);
+		displayboard(board, ROW, COL);
+		ret = iswin(board,ROW,COL);
+		if (ret != 'c');
+		{
+			break;
+		}
+		//判断电脑是否赢
+		if (ret == '*')
+			printf("玩家赢\n");
+		else if (ret == '#')
+			printf("电脑赢\n");
+		else if (ret == 'q')
+			printf("平局\n");
+	}
+
 }
 
 void menu()
@@ -26,6 +58,7 @@ void menu()
 void test()
 {
 	int input;
+	srand((unsigned int)time(NULL));
 	do
 	{
 		menu();
@@ -35,7 +68,7 @@ void test()
 		{
 		case 1:
 			game();
-				printf("三子棋\n");
+				//printf("三子棋\n");
 			break;
 		case 0:
 			printf("退出游戏");
